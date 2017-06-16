3d34633ad71fe6f30f0c235c48c22666f2bd056c
Surim Kim <kimsuelim@gmail.com>
2017-06-16 11:19:29 +0900

    improve test

--- test/config_test.rb
+++ test/config_test.rb
@@ -1,22 +1,31 @@
 require 'test_helper'
 
 class ConfigTest < Minitest::Spec
-  it "create a new pixabay client through initializer" do
+  before do
     Pixabay.configure do |config|
-      config.key = "key"
+      config.key = "1792764-8d5ef6920d5ab12a2e0c421f5"
       config.timeout = 10
     end
-    client = Pixabay.new
-    Pixabay::Client.must_equal client.class
+
+    @client = Pixabay.new
+  end
+
+  it "create a new pixabay client through initializer" do
+    @client.instance_variable_get(:@key).must_equal "1792764-8d5ef6920d5ab12a2e0c421f5"
+    @client.instance_variable_get(:@timeout).must_equal 10
   end
 
   it "search photos" do
-    client = Pixabay.new
-    client.photos(q: "yellow+flower", safesearch: true, page: 1, per_page: 30)
+    res = @client.photos(q: "yellow+flower", safesearch: true, page: 1, per_page: 30)
+    res["hits"].must_be_instance_of Array
+    res["total"].wont_be_nil
+    res["totalHits"].wont_be_nil
   end
 
   it "search videos" do
-    client = Pixabay.new
-    client.videos(q: "yellow+flower", safesearch: true, page: 1, per_page: 30)
+    res = @client.videos(q: "yellow+flower", safesearch: true, page: 1, per_page: 30)
+    res["hits"].must_be_instance_of Array
+    res["total"].wont_be_nil
+    res["totalHits"].wont_be_nil
   end
 end
