f4cb673cf57560be8ba9b26c2c0c9a16fbb15f08
Daan van Vugt <daan@vanvugt.me>
2021-05-11 17:31:07 +0200

    Add / to pixabay URL; rubocop

--- lib/pixabay.rb
+++ lib/pixabay.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'pixabay/config'
 require 'pixabay/version'
 
@@ -12,7 +14,7 @@ module Pixabay
 
   class Client
     include HTTParty
-    base_uri "https://pixabay.com/api"
+    base_uri 'https://pixabay.com/api/'
 
     def initialize(key: nil, timeout: 10)
       @key = key
@@ -21,26 +23,26 @@ module Pixabay
 
     def photos(**options)
       default_options = {
-        q: nil, lang: "en", id: nil,
-        response_group: "image_details", image_type: "all", orientation: "all", category: nil,
+        q: nil, lang: 'en', id: nil,
+        response_group: 'image_details', image_type: 'all', orientation: 'all', category: nil,
         min_width: 0, min_height: 0, editors_choice: false, safesearch: false,
-        order: "popular", page: 1, per_page: 20, callback: nil, pretty: false
+        order: 'popular', page: 1, per_page: 20, callback: nil, pretty: false
       }
 
       options = { query: { key: @key }.merge(options) }
-      self.class.get("", options)
+      self.class.get('', options)
     end
 
     def videos(**options)
       default_options = {
-        q: nil, lang: "en", id: nil,
-        video_type: "all", category: nil,
+        q: nil, lang: 'en', id: nil,
+        video_type: 'all', category: nil,
         min_width: 0, min_height: 0, editors_choice: false, safesearch: false,
-        order: "popular", page: 1, per_page: 20, callback: nil, pretty: false
+        order: 'popular', page: 1, per_page: 20, callback: nil, pretty: false
       }
 
       options = { query: { key: @key }.merge(options) }
-      self.class.get("/videos", options)
+      self.class.get('/videos', options)
     end
   end
 end
